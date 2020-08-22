d190ac5b37073240f8793faaf93a1e726054bf15
Haran Arbel <haranarbel@MacBook-Pro.local>
2020-08-22 21:40:04 +0300

    app working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Link } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import serializeForm from 'form-serialize'
@@ -7,43 +7,41 @@ import PageLayout from './PageLayout'
 import LoginButton from './LoginButton'
 import LogoutButton from './LogoutButton'
 import Profile from './Profile'
-import callSecureApi from './CallSecureApi'
 
 import * as API from './API'
 
 import './App.css';
-
-// import { useAuth0 } from "@auth0/auth0-react";
-// const { getAccessTokenSilently } = useAuth0();
-//
-// const callSecureApi = async () => {
-//     const apiUrl = "http://127.0.0.1:5000"
-//     const token = await getAccessTokenSilently();
-//
-//         const response = await fetch(`${apiUrl}/get_users`, {
-//             headers: {
-//                 Authorization: `Bearer ${token}`,
-//             },
-//         });
-//   };
+import { useAuth0 } from "@auth0/auth0-react";
 
 
-class App extends Component {
 
-  state = {
-      username: ''
-  }
 
-  handleSubmit = (e) => {
+function App() {
+
+  const [username, setUsername] = useState('');
+
+  const handleSubmit = (e) => {
         e.preventDefault()
         const values = serializeForm(e.target, {hash: true})
         console.log('values', values)
         // call API create user function
         // API.get_messages().then(msg => this.setState({msg: msg}))
-        API.create_user(values.name).then(username => this.setState({username: username}))
+        API.create_user(values.name).then(username => setUsername(username))
 
   }
 
+  const { getAccessTokenSilently } = useAuth0();
+  const callSecureApi = async () => {
+
+    const apiUrl = "http://127.0.0.1:5000"
+    const token = await getAccessTokenSilently();
+        console.log(token)
+        const response = await fetch(`${apiUrl}/get_users`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+  };
 
   // componentDidMount() {
   //   API.get_hello_worl_msg()
@@ -53,8 +51,7 @@ class App extends Component {
   //       }))
   //     })
   // }
-  render() {
-    return (
+  return (
         <div>
             <LoginButton></LoginButton>
             <LogoutButton></LogoutButton>
@@ -62,25 +59,24 @@ class App extends Component {
 
             {/*<PageLayout></PageLayout>*/}
             <Route exact path='/' render={() => (
-    <div>
-        <p className='primary'>Hello {this.state.username}!</p>
-        <Rater></Rater>
-        <Link to={'get_users'} onClick={callSecureApi}>ClickMe</Link>
-    </div>
-)}/>
+            <div>
+                <p className='primary'>Hello {username}!</p>
+                <Rater></Rater>
+                <Link to={'get_users'} onClick={callSecureApi}>ClickMe</Link>
+            </div>
+            )}/>
             <Route exact path='/get_users' render={() => (
-    <div>
-        <form onSubmit={this.handleSubmit}>
-            <input type='text' name='name' placeholder='Name'></input>
-            <button type>Add user</button>
-        </form>
-        <p>User is: {this.state.username}</p>
-        <Link to={'/'}>Back</Link>
-    </div>
-)}/>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input type='text' name='name' placeholder='Name'></input>
+                    <button type>Add user</button>
+                </form>
+                <p>User is: {username}</p>
+                <Link to={'/'}>Back</Link>
+            </div>
+        )}/>
         </div>
     )
-  }
 
 }
 
