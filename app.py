4559efecf32f591d37699a1205f295977f9aeff1
Haran Arbel <haranarbel@MacBook-Pro.local>
2020-11-15 18:27:50 +0200

    trying to change the favicon

--- app.py
+++ app.py
@@ -26,20 +26,20 @@ CORS(app)
 def index():
     return app.send_static_file('index.html')
 
-
-@app.route('/favicon.ico')
-def favicon():
-    return app.send_static_file('favicon.ico')
+#
+# @app.route('/favicon.ico')
+# def favicon():
+#     return app.send_static_file('favicon.ico')
 
 #
 # app.add_url_rule('/favicon.ico',
 #                  redirect_to=url_for('static', filename='favicon.ico'))
 
-#
-# @app.route('/favicon.ico')
-# def favicon():
-#     # app.send_static_file('favicon.ico')
-#     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/favicon.ico')
+def favicon():
+    # app.send_static_file('favicon.ico')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/users/<string:user_id>/role')
