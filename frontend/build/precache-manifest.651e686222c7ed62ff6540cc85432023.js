1fed49ad01a3b62c6b41e8d6789bfb94a6c328e6
Haran Arbel <haranarbel@MacBook-Pro.local>
2020-11-15 18:53:05 +0200

    put favicom in comment in index.html

---
+++ frontend/build/precache-manifest.651e686222c7ed62ff6540cc85432023.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626925e2492203878c69def708aa0116",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65975ac97355667d2d60",
+    "url": "/static/css/main.23afb749.chunk.css"
+  },
+  {
+    "revision": "de4d9db472c48841f417",
+    "url": "/static/js/2.0e6e2410.chunk.js"
+  },
+  {
+    "revision": "4d3d17ca1d72509d468eda11f7e92a3e",
+    "url": "/static/js/2.0e6e2410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65975ac97355667d2d60",
+    "url": "/static/js/main.4b22b95b.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
+    "url": "/static/media/arrow-back.c0c5c509.svg"
+  },
+  {
+    "revision": "576f26ff09a3275d9da0621e6b34898d",
+    "url": "/static/media/cancel.576f26ff.svg"
+  },
+  {
+    "revision": "c9c2fd159a339e59c77962878b1676bd",
+    "url": "/static/media/person-add.c9c2fd15.svg"
+  },
+  {
+    "revision": "1456a6be906dcfe7818c47b28b409501",
+    "url": "/static/media/person.1456a6be.svg"
+  },
+  {
+    "revision": "0fc053ba9ccf6f9d63d45098678875bf",
+    "url": "/static/media/search.0fc053ba.svg"
+  }
+]);
